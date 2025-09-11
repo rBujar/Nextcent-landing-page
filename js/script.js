daa309b63b7dda56b406b5c4020bccbe730e0597
Bujar Roka <rokabujar@gmail.com>
2025-09-11 12:06:54 +0200

    added animations and scroll to the top button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const hamburger = document.querySelector(".hamburger");
 const headerMenu = document.querySelector(".header__menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     headerMenu.classList.toggle("active");
+})
+
+const scrollButton = document.getElementById('scroll-top');
+
+window.onscroll = function() {scrollFunction()}
+
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        scrollButton.style.display = 'flex';
+    }else{
+        scrollButton.style.display = 'none';
+    }
+}
+
+// function topFunction(){
+//     document.documentElement.scrollTo({ top: 0, behavior: 'smooth'})
+// }
+    scrollButton.addEventListener("click", () => {
+        document.documentElement.scrollTo({ top: 0, behavior: 'smooth'})
+    })
+
+///AOS
+AOS.init({
+    duration: 1000,
+    offset: 100,
 })
