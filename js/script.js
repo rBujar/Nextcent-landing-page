22e7e58d7173a04e797123ee1a9891b6bd68cd8f
Bujar Roka <rokabujar@gmail.com>
2025-12-26 19:14:15 +0100

    cleaned up the code & restyled the header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-const hamburger = document.querySelector(".hamburger");
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const hamburger = document.querySelector(".header__hamburger");
 const headerMenu = document.querySelector(".header__menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     headerMenu.classList.toggle("active");
 })
+
+const scrollButton = document.getElementById('scroll-top');
+
+window.onscroll = function() {scrollFunction()}
+
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        scrollButton.style.display = 'flex';
+    }else{
+        scrollButton.style.display = 'none';
+    }
+}
+
+// function topFunction(){
+//     document.documentElement.scrollTo({ top: 0, behavior: 'smooth'})
+// }
+    scrollButton.addEventListener("click", () => {
+        document.documentElement.scrollTo({ top: 0, behavior: 'smooth'})
+    })
+
+///AOS
+AOS.init({
+    duration: 700,
+    offset: 50,
+    once: true,
+});
